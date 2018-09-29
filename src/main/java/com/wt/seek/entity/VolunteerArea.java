3bf3c3aee462c5ff05ccd50093a02e3e9e9da5d9
cgrezo <2422687506@qq.com>
2018-09-29 19:49:23 +0800

    志愿者地址修改

---
+++ src/main/java/com/wt/seek/entity/VolunteerArea.java
@@ -0,0 +1,68 @@
+package com.wt.seek.entity;
+
+public class VolunteerArea {
+	private int id;
+	private Integer provinceId;
+	private String provinceName;// 省份
+	private Integer cityId;
+	private String cityName;// 城市
+	private Integer countyId;
+	private String countyName;// 区县
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Integer getProvinceId() {
+		return provinceId;
+	}
+
+	public void setProvinceId(Integer provinceId) {
+		this.provinceId = provinceId;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+	public Integer getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(Integer cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public Integer getCountyId() {
+		return countyId;
+	}
+
+	public void setCountyId(Integer countyId) {
+		this.countyId = countyId;
+	}
+
+	public String getCountyName() {
+		return countyName;
+	}
+
+	public void setCountyName(String countyName) {
+		this.countyName = countyName;
+	}
+
+}
